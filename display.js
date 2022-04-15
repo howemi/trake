2ecba68b5330cdb3d1663b6121aac996599083fe
Michael Howe <michael.howe25@gmail.com>
2022-04-14 20:37:51 -0500

    Make boxes

--- display.js
+++ display.js
@@ -1,4 +1,9 @@
 let graph = document.getElementById("graph");
+for (let i = 0; i < 365; i++) {
+  let add = document.createElement("div");
+  add.classList.add("box");
+  graph.appendChild(add);
+}
 
 chrome.storage.sync.get("color", ({ color }) => {
   graph.style.backgroundColor = color;
